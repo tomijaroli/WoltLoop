af2c7b43db1619261283af1e953c819136212f62
tomjaroli <tamas.jaroli@gmail.com>
2022-03-14 11:23:59 +0100

    Initial Commit

---
+++ Wolt Loop/Wolt_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wolt_LoopApp.swift
+//  Wolt Loop
+//
+//  Created by Tom Jaroli on 2022. 03. 14..
+//
+
+import SwiftUI
+
+@main
+struct Wolt_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
